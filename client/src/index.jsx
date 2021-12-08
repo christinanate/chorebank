89e35305386e94056fa27808aaca3a40a52bc4b2
Christina Nathan <christina.a.nate@gmail.com>
2021-12-08 13:42:30 -0800

    set up 3 containers

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,7 +5,20 @@ import axios from 'axios';
 
 class App extends React.Component {
   render() {
-    return <h2>All set up!</h2>
+    return (
+      <div>
+        <h1 className='title'>Chore Bank</h1>
+        <div className='container-1'>
+          <div className='container-2'>
+            <h2>container 2</h2>
+          </div>
+          <div className='container-3'>
+            <h2>container 3</h2>
+          </div>
+        </div>
+      </div>
+
+    )
   }
 };
 
