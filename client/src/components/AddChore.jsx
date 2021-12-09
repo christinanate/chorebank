151b3555c6ecd7f7a14c3f5d0c6fed1d633a3f7f
Christina Nathan <christina.a.nate@gmail.com>
2021-12-08 19:18:58 -0800

    started getValues in addChore

--- client/src/components/AddChore.jsx
+++ client/src/components/AddChore.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
 //add chore to the database
+const getValues = (e) => {
+  e.preventDefault();
+  console.log('getValues invoked!');
+  let chore = e.target[0].value;
+  let points = e.target[1].value;
+  let createdBy = e.target[2].value;
+  console.log(chore, points, createdBy);
+  console.log(new Date().toISOString());
+  let createdDate = new Date().toISOString();
+
+  axios.post('/addChore', {
+    chore, points, createdBy, createdDate
+  })
+    .then(response => {
+      console.log(response);
+      //invoke p
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 let AddChore = () => {
   return (
-    <form>
+    <form onSubmit={getValues}>
       <label>Chore:
         <input type='text'></input>
       </label>
+      <label>$
+        <input type='text'></input>
+      </label>
+      <br></br>
       <label>Created By:
         <input type='text'></input>
       </label>
--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const { Chore, Account } = require('../database/index.js');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
 app.use(express.static('client/dist'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/addChore', (req, res) => {
+  console.log('req.body: ', req.body);
+  const { chore, points, createdBy, createdDate } = req.body;
+  res.end();
+});
 
 app.listen(port, () => {
   console.log(`ChoreBank listening at http://localhost:${port}`)
