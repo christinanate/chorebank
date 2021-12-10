1ffa9345bb4dd65f0a97d94a0726a172e46fb384
Christina Nathan <christina.a.nate@gmail.com>
2021-12-10 13:10:42 -0800

    positioned chore tickets

--- client/src/components/ChoreItem.jsx
+++ client/src/components/ChoreItem.jsx
@@ -37,11 +37,11 @@ let ChoreItem = ({ chore, getPendingChores }) => {
   return (
     <div className='choreitem-container'>
       <form onSubmit={handleSubmit}>
-        <label>created by: {chore.createdBy}</label>
-        <br></br>
-        <label>date: {chore.createdDate}</label>
-        <br></br>
-        <label>chore: {chore.chore}</label>
+        <div className='chore-item-header'>
+          <label>{chore.createdBy} asks...</label>
+          <br></br>
+        </div>
+        <label>{chore.chore}</label>
         <br></br>
         <label>${chore.points}</label>
         <br></br>
